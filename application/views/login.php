95f90179303f0516ba0b6d1089e19f28c6990b80
Cristian Ospina <crisospina523@gmail.com>
2019-05-19 14:02:12 -0500

    Se agrego el favicon del sitio

--- application/views/login.php
+++ application/views/login.php
@@ -11,6 +11,7 @@
 
   <title><?php echo $tituloLogin ?></title>
 
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url();?>/assets/img/favicon.png">
   <!-- Custom fonts for this template-->
   <link href="<?php echo base_url();?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
