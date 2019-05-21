460169f3c31ab707ab2de5ac584603509de0ac70
Cristian Ospina <crisospina523@gmail.com>
2019-05-21 15:10:26 -0500

    CRUD modulo historial del paciente

--- application/views/componentes/sidebar.php
+++ application/views/componentes/sidebar.php
@@ -53,7 +53,7 @@
 
     <!-- Nav Item - Utilities Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+        <a class="nav-link collapsed" href="<?php echo base_url();?>./index.php/historial" aria-expanded="true" aria-controls="collapseUtilities">
             <i class="fas fa-fw fa-heartbeat"></i>
             <span>Historial</span>
         </a>
